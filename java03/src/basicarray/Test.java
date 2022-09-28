e5e7a9eddf7311ab7102e50fc09fee21f36af6aa
kyeongseo <keongseo1234@gmail.com>
2022-09-28 17:47:05 +0900

    basicarray

---
+++ java03/src/basicarray/Test.java
@@ -0,0 +1,16 @@
+package basicarray;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    int[] answer = { 1, 1, 2, 3 }; 
+	    System.out.println(answer);
+
+	    int[] myanswer = { 1, 2, 4, 3 };
+	    
+	    int[] space = new int[2];
+
+	}
+
+}
